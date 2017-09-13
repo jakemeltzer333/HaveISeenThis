655312d79de11d391a932ee632bc6a6e2edbb390
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-13 13:19:07 -0400

    establish relationships between models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,3 @@
 class Movie < ApplicationRecord
-    belongs_to :user
+    belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates_uniqueness_of :username
     has_secure_password
+
+    has_many :movies
 end
