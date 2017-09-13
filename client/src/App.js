f61e69a6c1831ac967381aa3e4b7054aaa4fb21b
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-13 17:12:28 -0400

    write auth functions for front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,124 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import axios from 'axios';
+
+import Auth from './modules/Auth';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      auth: Auth.isUserAuthenticated(),
+      shouldFireRedirect: false,
+      loginUserName: '',
+      loginPassword: '',
+      registerUserName: '',
+      registerPassword: '',
+      registerEmail: '',
+      registerName: '',
+    }
+  }
+
+  handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleLoginSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/login', {
+      username: this.state.loginUserName,
+      password: this.state.loginPassword,
+    }).then(res => {
+      console.log(res);
+      if (res.data.token) {
+        Auth.authenticateToken(res.data.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+          loginUserName: '',
+          loginPassword: '',
+        })
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  handleRegisterSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/users', {
+      user: {
+        username: this.state.registerUserName,
+        password: this.state.registerPassword,
+        email: this.state.registerEmail,
+        name: this.state.registerName,
+      }
+    }).then(res => {
+      if (res.data.token) {
+        Auth.authenticateToken(res.data.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+        })
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  resetFireRedirect = () => {
+    if (this.state.shouldFireRedirect) {
+      this.setState({
+        shouldFireRedirect: false,
+      })
+    }
+  }
+
+  logoutUser = () => {
+    axios.delete('/logout', {
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken()
+      }
+    }).then(res => {
+      Auth.deauthenticateUser();
+      this.setState({
+        auth: Auth.isUserAuthenticated(),
+        loginUserName: '',
+        loginPassword: '',
+      })
+    })
+  }
+
   render() {
     return (
+      <Router>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path = '/login' render={() => 
+        !this.state.auth ? (<LoginForm auth= {this.state.auth}
+                              loginUserName= {this.state.loginUserName}
+                              loginPassword= {this.state.loginPassword}
+                              handleInputChange= {this.handleInputChange}
+                              handleLoginSubmit= {this.handleLoginSubmit} />
+          ) : (
+            < Redirect to='/profile'/>
+          )}
+        />
+         <Route exact path = '/profile' render={() => 
+         this.state.auth ? <Profile auth={this.state.auth} resetFireRedirect={this.resetFireRedirect} /> : <Redirect to="/login"/> 
+        } />
       </div>
+      </Router>
     );
   }
 }
