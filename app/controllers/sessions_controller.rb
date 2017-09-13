3187b15aa2a2769eba75775add2dcfa0ed3b932e
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-13 13:36:24 -0400

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApiController
+    skip_before_action :require_login, only: [:create], raise: false
+
+    def create
+        if user = User.validate_login(params[:username], params[:password])
+            allow_token_to_be_used_only_once_for(user)
+            send_token_for_valid_login_of(user)
+        else
+            render_unauthorized("Something Wrong With Your Login or Password")
+        end
+    end
+
+    def destroy
+        logout
+        head :ok
+    end
+
+    private
+
+    def send_token_for_valid_login_of
+        render json: { token: user.auth_token }
+    end
+
+    def allow_token_to_be_used_only_once_for
+        user.regenerate_auth_token
+    end
+
+    def logout
+        current_user.invalidate_token
+    end
+end
