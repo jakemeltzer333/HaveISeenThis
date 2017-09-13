e55998a02b960ad8700af7b2c7569316a42e4de7
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-13 09:41:41 -0400

    create users table/model

---
+++ db/migrate/20170913133728_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, index: true
+      t.string :password_digest
+      t.string :email
+      t.string :auth_token
+      t.string :name
+      t.timestamps
+    end
+  end
+end
