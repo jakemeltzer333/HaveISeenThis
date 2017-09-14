3c0d4ac94045ee08a8291fc5aabf653ed7295f11
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-14 18:16:47 -0400

    make nav component

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import Auth from '../modules/Auth';
+
+const Nav = props => {
+    return (
+        <div className= 'nav'>
+            <ul>
+                <li><Link to = '/'>Home</Link></li>
+                {!Auth.isUserAuthenticated() ? <li> 
+                    <Link to = '/login'>Login</Link>
+                </li> : ''}
+                {!Auth.isUserAuthenticated() ? <li>
+                    <Link to = '/register'>Register</Link>
+                    </li> : ''}
+                {Auth.isUserAuthenticated() ? <li>
+                    <Link to = '/movies'>Seen Movies</Link>
+                    </li> : ''}    
+                {Auth.isUserAuthenticated() ? <li>
+                    <Link to = '/' onClick={props.logoutUser}>Logout</Link>
+                    </li> : ''}
+                   
+            </ul>    
+        </div>    
+    )
+}
+
+export default Nav;
