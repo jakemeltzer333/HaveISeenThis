e38adfaa686e255219bc9cf8e19c6558bccdd60c
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-14 12:07:27 -0400

    update user model to include relationship to seen movies table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_secure_password
     has_secure_token :auth_token
 
-    has_many :movies
+    has_many :movies through :seenmovies
 
     def invalidate_token
         self.update_columns(auth_token: nil)
