afbc2de127899c2a2c9751477c68168e108028a1
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-14 18:16:32 -0400

    make header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav.jsx';
+
+const Header = props => {
+    return (
+        <div className= 'header'>
+           <h1>Have I Seen This?</h1> 
+            <Nav logoutUser={props.logoutUser}/>
+        </div>    
+    )
+}
+
+export default Header;
