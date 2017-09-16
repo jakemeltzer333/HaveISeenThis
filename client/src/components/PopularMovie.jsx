52f0d8363fd6d2e20c230700a93c656807289d3e
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-16 17:20:54 -0400

    make PopularMovie component for each movie that shows up on the home page

---
+++ client/src/components/PopularMovie.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const PopularMovie = props => {
+         let url= props.posterResults.secure_base_url
+         let imageSize= 'w185'
+         let posterPath= props.movie.poster_path
+         let poster= `${url}${imageSize}${posterPath}`
+
+         
+        return (
+                <div className='movie-poster-result'>
+                <Link to={`/movies/${props.movie.id}`} 
+                    onClick={() => {props.handleSingleMovie(props.movie.id)}}><img src={poster} alt ={props.movie.title}/></Link>
+                 </div>    
+             )
+}
+
+export default PopularMovie;
