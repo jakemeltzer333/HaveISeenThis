0c31c5e3587b1bdb00215a07e963669d39d0aa57
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-16 18:40:03 -0400

    get poster image to render in each single movie page

--- client/src/components/SingleMovie.jsx
+++ client/src/components/SingleMovie.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const SingleMovie = props => {
+
+    let url= props.posterResults.secure_base_url
+    let imageSize= 'w185'
+    let posterPath= props.movieData.poster_path
+    let poster= `${url}${imageSize}${posterPath}`
+
     return (
         <div>
+            <img src={poster}/>
             <h1>{props.movieData.title}</h1>
             <h5>Tagline: {props.movieData.tagline}</h5>
-            <p>Synopsis: {props.movieData.overview}</p>
+            <h5>Synopsis:</h5> <p>{props.movieData.overview}</p>
             <h5>Runtime: {props.movieData.runtime} Minutes</h5>
             <h5>Release Date: {props.movieData.release_date}</h5>
         </div>    
