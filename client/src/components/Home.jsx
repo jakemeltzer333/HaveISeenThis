d6aee415d4bf4451a2c65dfb27760d0272390ab7
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-16 17:21:38 -0400

    refactor renderMoviesList function to include PopularMovie component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 import SearchResult from './SearchResult.jsx'
+import PopularMovie from './PopularMovie.jsx'
 
 class Home extends Component {
 
@@ -10,7 +11,8 @@ class Home extends Component {
          if(this.props.searchLoaded) {
              return this.props.searchResults.map(searchedMovie => {
                  return (
-                     <SearchResult key={searchedMovie.id} posterResults={this.props.posterResults} searchedMovie={searchedMovie}/>
+                     <SearchResult key={searchedMovie.id} posterResults={this.props.posterResults} 
+                      searchedMovie={searchedMovie} handleSingleMovie={this.props.handleSingleMovie}/>
                  )
              })
          }
@@ -20,12 +22,8 @@ class Home extends Component {
     if(this.props.apiDataLoaded) {
                 return this.props.apiData.map(movie => {
                     return (
-                       <div className='popular-movie-info'> 
-                        <h4><Link to={`/movies/${movie.id}`}
-                            onClick={() => {this.props.handleSingleMovie(movie.id)}}>{movie.title}</Link></h4>
-                        <p>Synopsis: {movie.overview}</p>
-                        <p>Release Date: {movie.release_date}</p>
-                       </div> 
+                       <PopularMovie key={movie.id} posterResults={this.props.posterResults}
+                        movie={movie} handleSingleMovie={this.props.handleSingleMovie} />
                     )
                 })
         } else {
