0c16272b129233118f2b17ad2d4a43bd15851bb2
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-16 18:47:40 -0400

    classes for elements in SingleMovie component

--- client/src/components/SingleMovie.jsx
+++ client/src/components/SingleMovie.jsx
@@ -8,13 +8,13 @@ const SingleMovie = props => {
     let poster= `${url}${imageSize}${posterPath}`
 
     return (
-        <div>
-            <img src={poster}/>
-            <h1>{props.movieData.title}</h1>
-            <h5>Tagline: {props.movieData.tagline}</h5>
-            <h5>Synopsis:</h5> <p>{props.movieData.overview}</p>
-            <h5>Runtime: {props.movieData.runtime} Minutes</h5>
-            <h5>Release Date: {props.movieData.release_date}</h5>
+        <div className='single-movie-container'>
+            <img className='single-poster' src={poster}/>
+            <h1 className='movie-title'>{props.movieData.title}</h1>
+            <h5 className='tagline'>Tagline: {props.movieData.tagline}</h5>
+            <h5 className='synopsis-header'>Synopsis:</h5> <p className='synopsis'>{props.movieData.overview}</p>
+            <h5 className='runtime'>Runtime: {props.movieData.runtime} Minutes</h5>
+            <h5 className='release-date'>Release Date: {props.movieData.release_date}</h5>
         </div>    
     )
 }
