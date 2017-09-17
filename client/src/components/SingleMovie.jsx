5a677b6f8bfa98932b3d0326119cb363f7d55f63
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-17 12:52:27 -0400

    add font awesome icons to mark if movie has been seen

--- client/src/components/SingleMovie.jsx
+++ client/src/components/SingleMovie.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import Auth from '../modules/Auth';
 
 const SingleMovie = props => {
 
     let url= props.posterResults.secure_base_url
-    let imageSize= 'w185'
+    let imageSize= 'w300'
     let posterPath= props.movieData.poster_path
     let poster= `${url}${imageSize}${posterPath}`
 
     return (
         <div className='single-movie-container'>
-            <img className='single-poster' src={poster}/>
+            <img className='single-poster' src={poster} alt=''/>
             <h1 className='movie-title'>{props.movieData.title}</h1>
             <h5 className='tagline'>Tagline: {props.movieData.tagline}</h5>
             <h5 className='synopsis-header'>Synopsis:</h5> <p className='synopsis'>{props.movieData.overview}</p>
             <h5 className='runtime'>Runtime: {props.movieData.runtime} Minutes</h5>
             <h5 className='release-date'>Release Date: {props.movieData.release_date}</h5>
+            <i className="fa fa-check-circle fa-2x" aria-hidden="true"> Hey, I've Seen This Movie!</i>
+            {Auth.isUserAuthenticated() ? 
+                <i className="fa fa-times-circle fa-2x" aria-hidden="true"> Wait, No I Haven't! Remove This!</i>
+                : ''}
         </div>    
     )
 }
