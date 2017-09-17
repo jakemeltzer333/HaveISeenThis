33c921fe21ee0ae357dddb473f892d00c596561f
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-17 12:51:03 -0400

    give nav list and list items classnames for styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 
-import { Link } from 'react-router-dom';
-
 import SearchResult from './SearchResult.jsx'
 import PopularMovie from './PopularMovie.jsx'
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,22 +5,22 @@ import Auth from '../modules/Auth';
 
 const Nav = props => {
     return (
-        <div className= 'nav'>
-            <ul>
-                <li><Link to = '/'>Home</Link></li>
-                {!Auth.isUserAuthenticated() ? <li> 
+        <div className= 'nav-container'>
+            <ul className='nav'>
+                <li className='nav-item'><Link to = '/'>Home</Link></li>
+                {!Auth.isUserAuthenticated() ? <li className='nav-item'> 
                     <Link to = '/login'>Login</Link>
                 </li> : ''}
-                {!Auth.isUserAuthenticated() ? <li>
+                {!Auth.isUserAuthenticated() ? <li className='nav-item'> 
                     <Link to = '/register'>Register</Link>
                     </li> : ''}
-                {Auth.isUserAuthenticated() ? <li>
+                {Auth.isUserAuthenticated() ? <li className='nav-item'>
                     <Link to = '/profile'>Profile</Link>
                     </li> : ''}    
-                {Auth.isUserAuthenticated() ? <li>
+                {Auth.isUserAuthenticated() ? <li className='nav-item'>
                     <Link to = '/movies'>Seen Movies</Link>
                     </li> : ''}    
-                {Auth.isUserAuthenticated() ? <li>
+                {Auth.isUserAuthenticated() ? <li className='nav-item'>
                     <Link to = '/' onClick={props.logoutUser}>Logout</Link>
                     </li> : ''}
                    
