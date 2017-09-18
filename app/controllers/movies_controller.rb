dc1bfc0b27ea2dadca43fd387e61c3c967e6956c
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-18 10:27:59 -0400

    have different methods to get all movies and all user's movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,29 @@
 
     class MoviesController < ApiController
-    before_action :require_login, except: [:index, :show, :search]
+    before_action :require_login, except: [:index_search, :show_search, :search]
     
     def index
+        movies = current_user.movies.all
+        render json: { movies: movies }
+    end
+    
+    def index_search
         movie_key = Rails.application.secrets.api_key
         poster_response = HTTParty.get("https://api.themoviedb.org/3/configuration?api_key=#{movie_key}")
-        response = HTTParty.get("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=#{movie_key}")    
+        response = HTTParty.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{movie_key}&language=en-US&page=1")    
         render json: {response: response, poster_response: poster_response} 
     end
 
     def show
+        movie = current_user.movies.find_by(params[:id])
+        render json: { movie: movie }
+    end
+
+    def show_search
         movie_key = Rails.application.secrets.api_key
         movie_id = params[:id]
         response = HTTParty.get("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{movie_key}")
-        render json: response
+        render json: { response: response }
     end
 
     def search
