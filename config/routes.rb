87f1e240f5cc1b95d961bffc82b69b155ccd7140
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-19 17:15:35 -0400

    deployment stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # root "movies#index"
+  root to: "root#index"
   
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
