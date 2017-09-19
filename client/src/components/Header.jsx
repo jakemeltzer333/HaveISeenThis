fed468969f615aa1603f95b029d0d8f2be06fc2c
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-19 15:48:05 -0400

    Change app name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav.jsx';
 const Header = props => {
     return (
         <div className= 'header'>
-           <h1 className='app-name'>Have I Seen This?</h1> 
+           <h1 className='app-name'>SeenIt?</h1> 
             <Nav logoutUser={props.logoutUser}/>
         </div>    
     )
