94bc15cf3706cec239643296cdef9670cc17d1c3
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-20 10:36:01 -0400

    style login and register pages

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Login = props => {
     return (
         <form className= 'login' onSubmit={props.handleLoginSubmit}>
-            <input className = 'input' type='text' name='loginUserName' value={props.loginUserName}
+            <input className = 'login-input' type='text' name='loginUserName' value={props.loginUserName}
             placeholder='Username' onChange={props.handleInputChange}/>
-            <input className = 'input' type='password' name='loginPassword' value={props.loginPassword}
+            <input className = 'login-input' type='password' name='loginPassword' value={props.loginPassword}
             placeholder='Password' onChange={props.handleInputChange} />
-            <input className='submit' type='submit' value='Log In' />
+            <input className='login-submit' type='submit' value='Log In' />
         </form>    
     )
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Register = props => {
     return (
         <form className= 'register' onSubmit={props.handleRegisterSubmit}>
-            <input className = 'input' type='text' name='registerUserName' value={props.registerUserName}
+            <input className = 'register-input' type='text' name='registerUserName' value={props.registerUserName}
             placeholder='Username' onChange={props.handleInputChange}/>
-            <input className = 'input' type='password' name='registerPassword' value={props.registerPassword}
+            <input className = 'register-input' type='password' name='registerPassword' value={props.registerPassword}
             placeholder='Password' onChange={props.handleInputChange} />
-            <input className = 'input' type='text' name='registerEmail' value={props.registerEmail}
+            <input className = 'register-input' type='text' name='registerEmail' value={props.registerEmail}
             placeholder='Email' onChange={props.handleInputChange} />
-            <input className = 'input' type='text' name='registerName' value={props.registerName}
+            <input className = 'register-input' type='text' name='registerName' value={props.registerName}
             placeholder='Name' onChange={props.handleInputChange} />
-            <input className='submit' type='submit' value='Register' />
+            <input className='register-submit' type='submit' value='Register' />
         </form>    
     )
 }
