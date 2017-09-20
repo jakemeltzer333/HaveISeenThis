e1b678cce84381b264e62c8ce39a04d2916422c6
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-20 09:03:02 -0400

    add api logo to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import logo from '../images/tmdblogo.png'
+
 const Footer = props => {
 
     return (
         <div className='footer'>
-            <img className='api-logo' src='../../images/tmdblogo.png' />
+            <img className='api-logo' src={logo} />
             <h5 className='disclosure'>This product uses the TMDb API but is not endorsed or certified by TMDb.</h5>
         </div>
     )
