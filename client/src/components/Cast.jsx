e43621e9aa582cacfebaaa36aa341df44e841551
Jacob Meltzer <jmeltzer33@gmail.com>
2017-10-17 14:00:58 -0400

    can render each cast member and their character names

--- client/src/components/Cast.jsx
+++ client/src/components/Cast.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
 class Cast extends Component {
+    renderCastList = () => {
+        if(this.props.movieCreditsDataLoaded) {
+            return this.props.movieCreditsData.cast.map(actor => {
+                console.log('this function works');
+                return (
+                    <div className='cast'>
+                        <p>{actor.name}: {actor.character}</p>
+                    </div>    
+                )
+            })
+        }
+    }
 
+    render () {
+        return (
+            <div className='cast-container'>
+                {this.renderCastList()}
+            </div>    
+        )
+    }
 }
 
 export default Cast;
